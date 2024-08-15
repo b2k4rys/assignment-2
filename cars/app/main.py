a000c8cd2e54f2c223f31c19afbd5f2641f71450
Bekarys <bekarys20050812@gmail.com>
2024-08-15 23:20:51 +0500

    finish

--- cars/app/main.py
+++ cars/app/main.py
@@ -1,9 +1,40 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, HTTPException
+import random
+from faker import Faker
+from faker_vehicle import VehicleProvider
 
-from .cars import create_cars
+
+def create_cars(cars_total):
+    fake = Faker()
+    fake.add_provider(VehicleProvider)
+
+    car_ids = list(range(cars_total))
+    random.seed(0)
+    random.shuffle(car_ids)
+
+    cars = [
+        {
+            "id": car_ids[i] + 1,
+            "name": fake.vehicle_make_model(),
+            "year": fake.machine_year(),
+        }
+        for i in range(cars_total)
+    ]
+
+    return cars
+
+
+
 
 cars = create_cars(100)  # Здесь хранятся список машин
 app = FastAPI()
+
+car_list = cars
+car_by_id = dict()
+
+for i in car_list:
+    car_by_id[i['id']] = i
+
 
 
 @app.get("/")
@@ -12,6 +43,17 @@ def index():
 
 
 # (сюда писать решение)
+
+@app.get('/cars')
+def cars(page: int = 1, limit: int = 10):
+    return car_list[page*limit-10:page*limit]
+
+@app.get('/cars/{id}')
+def cars(id: str):
+    if int(id) in car_by_id.keys():
+        return car_by_id[int(id)]
+    else:
+        raise HTTPException(status_code=404, detail="Item not found")
 
 
 # (конец решения)
--- users/app/main.py
+++ users/app/main.py
@@ -1,13 +1,43 @@
 from fastapi import FastAPI, Request, Response
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+import random
 
-from .users import create_users
+from faker import Faker
+
+
+def create_users(total):
+    fake = Faker()
+    Faker.seed(1)
+
+    ids = list(range(total))
+    random.seed(1)
+    random.shuffle(ids)
+
+    elements = [
+        {
+            "id": ids[i] + 1,
+            "email": fake.free_email(),
+            "first_name": fake.first_name(),
+            "last_name": fake.last_name(),
+            "username": fake.user_name(),
+        }
+        for i in range(total)
+    ]
+
+    return elements
+
+
 
 users = create_users(100)  # Здесь хранятся список пользователей
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
+users_by_id = dict()
 
+for i in users:
+    users_by_id[i['id']] = i
+print(users_by_id[18])
 
 @app.get("/")
 def index(request: Request):
@@ -16,6 +46,13 @@ def index(request: Request):
 
 # (сюда писать решение)
 
+@app.get('/users/{id}', response_class=HTMLResponse)
+async def get_user(request: Request, id: str):
+    user = users_by_id[int(id)]
+    return templates.TemplateResponse(name="users/user.html", context={"user": user, "request": request})
 
+@app.get('/users', response_class=HTMLResponse)
+async def get_users(request: Request):
+    return templates.TemplateResponse(name='users/index.html', context={'users':users, 'request':request})
 
 # (конец решения)
